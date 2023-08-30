3fb9937f1d75d3b8e6b36abd954e9216efd60daf
papon-nri <papon.dumrhidee@nri.com>
2023-08-30 14:05:20 +0700

    remove rdme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,5 @@
-// import './globals.css'
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
 import { globalStyles } from "@/stitches.config";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "KOLNECT",
